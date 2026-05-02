ac1ae973072b3dc88dafda96952e2bb4a51893ef
Olia <olyasemenchuk13@gmail.com>
2026-05-02 23:48:51 +0300

    installed flatpickr

---
+++ src/js/1-timer.js
@@ -0,0 +1,15 @@
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+flatpickr ('#datetime-picker', options)
